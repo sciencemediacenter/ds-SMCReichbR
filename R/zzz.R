105c73fd847cbadb618e88f05bc3de2b9b40f8c4
Simon Essink <simon.essink@sciencemediacenter.de>
2026-02-02 15:39:10 +0100

    fix: add missing Id import from DBI package
    The Id() function from DBI was being used in Geofilter.R but not imported,
    causing 'could not find function Id' errors. Added Id to @importFrom DBI directive.

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 #' @keywords internal
 #' @importFrom dplyr mutate left_join summarise filter distinct bind_rows rowwise as_tibble select pull inner_join tbl collect tibble
 #' @importFrom readr read_csv read_csv2 write_csv cols col_character
-#' @importFrom DBI dbIsValid dbExecute dbWriteTable dbGetQuery dbAppendTable dbListTables dbDisconnect dbConnect dbExistsTable dbGetInfo
+#' @importFrom DBI dbIsValid dbExecute dbWriteTable dbGetQuery dbAppendTable dbListTables dbDisconnect dbConnect dbExistsTable dbGetInfo Id
 #' @importFrom connections connection_view
 #' @importFrom RPostgres Postgres
 #' @importFrom duckdb duckdb
